e2b9cd3623a4a1890eadcb4bf502eb0ad601d3d6
Ryu@Vaio <ryutaroy@vaio>
2019-05-18 09:23:51 +0900

    search

---
+++ search/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args){
+        String strA = "abc";
+        String strB = "abe";
+
+        String[] arrayStrA = strA.split("");
+        String[] arrayStrB = strB.split("");
+
+        System.out.println(222222222);
+       System.out.println(Relaventsearch.isRelaventScore(strA,strB));
+    }
+}
---
+++ search/src/testUtil.java
@@ -0,0 +1,7 @@
+public class testUtil {
+    public static void sysoutArray(String[] array){
+        for(String str : array){
+            System.out.println(str);
+        }
+    }
+}
